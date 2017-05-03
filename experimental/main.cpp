81fe9415708aa9c591015f145ece31581027145f
Sergey Koparov <sergeykoparov@gmail.com>
2017-05-04 00:12:12 +0300

    Update tests

--- experimental/main.cpp
+++ experimental/main.cpp
@@ -1,138 +1,165 @@
+#include "stdafx.h"
+
 #include <iostream>
 #include <string>
 #include <stdexcept>
 #include <chrono>
 #include <cassert>
-#include "time_info/time_info.h"
+#include "time_info/time_ratio.h"
 
 void time_moment_date_time_test()
 {
-    using namespace time_info;
+	using namespace time_info;
+    using namespace time_info::details;
 
-    time_moment t1{ 1, 0 };
-    time_moment t2{ 1, 0 };
-    time_moment t3{ 1, 1 };
-    time_moment t4{ 2, 1 };
+	time_moment t1{ 1, 999999999 };
+	time_moment t2{ 1, 999999999 };
+	time_moment t3{ 2, 1 };
 
-    assert( t1 == t1.to_date_time().to_time_moment() );
+	assert(t1 == t2);
+	assert(t1 != t3);
+	assert(t1 < t3);
+	assert(t3 > t1);
+	assert(t1 <= t1 && t1 <= t2 && t1 <= t3);
+	assert(t3 >= t1 && t3 >= t2 && t3 >= t1);
+	assert((t1 + t2 == time_moment{ 3, 999999998 }));
+	assert((t1 - t2 == time_moment{ 0, 0 }));
+	assert((t3 - t2 == time_moment{ 0, 2 }));
 
-    assert( t1 == t2 );
-    assert( t1 != t3 && t1 != t4 );
-    assert( t1 < t3 && t1 < t4 );
-    assert( t4 > t3 && t4 > t2 && t4 > t1 );
-    assert( t1 <= t2 && t1 <= t3 && t1 <= t4 );
-    assert( t4 >= t1 && t4 >= t2 && t4 >= t3 && t4 >= t4 );
+	date_time d1{ 2015, date_time::apr, 24, 13, 13, 13, 13 };
+	date_time d2{ 2015, date_time::apr, 24, 13, 13, 13, 13 };
+	date_time d3{ 2016, date_time::apr, 25, 13, 13, 13, 13 };
+	date_time d4{ 2016, date_time::jan, 26, 13, 13, 13, 13 };
+	date_time d5{ 2016, date_time::dec, 27, 13, 13, 13, 13 };
 
-    date_time d1{ 2015, date_time::apr, 24, 13, 13, 13, 13, 13 };
-    date_time d2{ 2015, date_time::apr, 24, 13, 13, 13, 13, 13 };
-    date_time d3{ 2016, date_time::apr, 25, 13, 13, 13, 13, 13 };
-    date_time d4{ 2016, date_time::jan, 26, 13, 13, 13, 13, 13 };
-    date_time d5{ 2016, date_time::dec, 27, 13, 13, 13, 13, 13 };
-
-    assert( d1 == d1.to_time_moment().to_date_time() );
-
-    assert( d1 == d2 );
-    assert( d1 < d3 && d1 < d4 && d1 < d5 );
-    assert( d1 <= d1 && d1 <= d2 && d1 <= d3 && d1 <= d4 && d1 <= d5 );
-    assert( d5 > d1 && d5 > d2 && d5 > d3 && d5 > d4 );
-    assert( d5 >= d1 && d5 >= d2 && d5 >= d3 && d5 >= d4 && d5 >= d5 );
+	assert(d1 == d2);
+	assert(d1 < d3 && d1 < d4 && d1 < d5);
+	assert(d1 <= d1 && d1 <= d2 && d1 <= d3 && d1 <= d4 && d1 <= d5);
+	assert(d5 > d1 && d5 > d2 && d5 > d3 && d5 > d4);
+	assert(d5 >= d1 && d5 >= d2 && d5 >= d3 && d5 >= d4 && d5 >= d5);
 }
 
 void time_info_test()
 {
-    using namespace time_info;
+	using namespace time_info;
+    using namespace time_info::details;
 
-    {
-        day d{ 2 };
-        hour h = d;
-        min m = d;
-        sec s = d;
-        msec ms = d;
+	{
+		days_t d{ 2 };
+		hours_t h{ d };
+		minutes_t m{ d };
+		seconds_t s{ d };
+		millisec_t ms{ d };
+		microsec_t mic{ d };
+		nanosec_t ns{ d };				
 
-        day d1{ 2 };
-        hour h1{ d };
-        min m1{ d };
-        sec s1{ d };
-        msec ms1{ d };
+		days_t d1{ d };
+		hours_t h1{ d };
+		minutes_t m1{ d };
+		seconds_t s1{ d };
+		millisec_t ms1{ d };
+		microsec_t mic1{ d };
+		nanosec_t ns1{ d };
 
-        assert( d.count() == 2 && d1.count() == 2 );
-        assert( h.count() == 48 && h1.count() == 48 );
-        assert( m.count() == 2880 && m1.count() == 2880 );
-        assert( s.count() == 172800 && s1.count() == 172800 );
-        assert( ms.count() == 172800000 && ms1.count() == 172800000 );
-    }
+		d1 -= h1;
+		d1 += h1;
 
-    {
-        time_moment tm{ 172800000, 10 };
-        day d{ tm };
-        assert( d.count() == 2 );
-        assert( d.to_time_moment() == tm );
-    }
+		assert(d == d1 && h == h1 && m == m1 && s == s1 && ms == ms1 && mic == mic1 && ns == ns1);
+		assert(d.count() == 2 && d1.count() == 2);
+		assert(h.count() == 48 && h1.count() == 48);
+		assert(m.count() == 2880 && m1.count() == 2880);
+		assert(s.count() == 172800 && s1.count() == 172800);
+		assert(ms.count() == 172800000 && ms1.count() == 172800000);
+		assert(mic.count() == 172800000000 && mic1.count() == 172800000000);
+		assert(ns.count() == 172800000000000 && ns1.count() == 172800000000000);
+	}
 
-    {
-        time_type ms_since_ep{ 1493307502629 };
+	{
+		time_t tt = 172800;
+		millisec_t ms;
+		ms.from_time_t(tt);
+		assert(tt == ms.to_time_t());
 
-        time_moment tm{ ms_since_ep, 10 };
-        date_time dt_right{ 2017, date_time::apr, 27, 15, 38, 22, 629, 10 };
+		timeval tv{ 172800, 999 };
+		hours_t h{ tv };
+		assert(h.to_timeval().tv_sec == tv.tv_sec && h.to_timeval().tv_usec == tv.tv_usec);
 
-        msec ms{ tm };
-        assert( ms.to_time_moment() == tm );
-        assert( ms.count() == ms_since_ep );
-        assert( ms.to_date_time() == dt_right );
+		timespec ts{ 172800, 999 };
+		minutes_t m{ ts };
+		auto tsss = m.to_timespec();
+		assert(m.to_timespec().tv_sec == ts.tv_sec && m.to_timespec().tv_nsec == ts.tv_nsec);
+	}
 
-        ms = dt_right;
-    }
+	{
+		time_moment tm{ 172800, 10 };
+		days_t d{ tm };
+		assert(d.count() == 2);		
+	}
+
+	{        
+		time_type ms_since_ep{ 1493307502629 };        
+		time_type ns{ 629000000 };
+
+		time_moment tm{ 0, ns };
+        tm.set_sec_epoch( ms_since_ep / 1000 );
+		date_time dt_right{ 2017, date_time::apr, 27, 15, 38, 22, 629000000 };
+
+		
+		millisec_t ms{ tm };
+		date_time a = ms.to_date_time();
+		auto count = ms.count();
+
+        static constexpr time_type julian_sec_before_epoch{ 2440588 * ratio::day_ratio_sec };
+
+		assert(ms.count() == ms_since_ep + julian_sec_before_epoch * 1000 );
+		assert(ms.to_date_time() == dt_right);
+
+        {
+            auto dayt = ms.to_date_time();
+            assert( dayt.year() == 2017 );
+            assert( dayt.month() == date_time::apr );
+            assert( dayt.day() == 27 );
+            assert( dayt.hour() == 15 );
+            assert( dayt.minute() == 38 );
+            assert( dayt.sec() == 22 );
+            assert( dayt.nsec() == 629000000 );
+        }
+
+        ms += days_t{ 365 };
+        auto dayt = ms.to_date_time();
+
+        nanosec_t tt{ dayt };
+        assert( dayt.year() == 2018 );
+        assert( dayt.month() == date_time::apr );
+        assert( dayt.day() == 27 );
+        assert( dayt.hour() == 15 );
+        assert( dayt.minute() == 38 );
+        assert( dayt.sec() == 22 );
+        assert( dayt.nsec() == 629000000 );
+               
+        {
+            nanosec_t nnns;
+            nnns.from_nsec_since_epoch();
+            auto nss3 = nnns.to_std_duration< std::chrono::nanoseconds >();
+            time_type ss1 = nnns.count() - julian_sec_before_epoch * 1000000000;
+            time_type ss2 = std::chrono::duration_cast< std::chrono::nanoseconds >( std::chrono::system_clock::now().time_since_epoch() ).count();
+            time_type ss3 = nss3.count();
+            assert( ( ss1 == ss2 && ss1 == ss3 ) );
+        }
+
+        {
+            std::chrono::nanoseconds mns{ 999999999 };
+            nanosec_t ns1;
+            ns1.from_std_duration( mns );
+            time_type traj = ns1.count_since_epoch();
+            auto mns1 = ns1.to_std_duration< std::chrono::nanoseconds >();
+            assert( traj == mns.count() && mns == mns1 );
+        }
+	}
 }
-
-
-int main(int argc, char **argv)
-{
-    time_moment_date_time_test();
-    time_info_test();
-
-    time_info::day day{ 1 };
-    time_info::hour hours = std::move( day );
-
-    hours = time_info::day{ 2 };
-    time_info::time_type hours_count{ hours.count() };
-
-    using namespace std::chrono;
-    milliseconds ms = duration_cast< milliseconds >(
-                system_clock::now().time_since_epoch());
-
-    auto countt = ms.count();
-
-    time_info::time_moment tp{ ms.count(), 0 };
-
-    time_info::date_time ddt = tp.to_date_time();
-    time_info::time_moment tp1 = ddt.to_time_moment();
-
-    time_info::hour h{ 1 };
-
-    int d = h.to_std_duration< std::chrono::minutes >().count();
-
-
-    std::tm tm = {};
-    const char* snext = ::strptime("1980-Jul-1 00:00:00.000000", "%Y-%b-%d %H:%M:%S", &tm);
-    auto time_point = std::chrono::system_clock::from_time_t(std::mktime(&tm));
-    long long  duration_ms = time_point.time_since_epoch() / std::chrono::milliseconds(1);
-    std::cout << duration_ms << std::endl;
-
-    time_info::date_time dt{ 2017, time_info::date_time::apr, 26, 16, 42, 52, 0, 0 };
-
-    time_info::hour hinfo{ dt };
-    size_t iii = hinfo.count();
-
-    time_info::hour ddd = time_info::hour::from_time_t< time_info::hour >( 3600 );
-    int t =ddd.count();
-
-    return EXIT_SUCCESS;
+int main()
+{		
+	time_info_test();
+	time_moment_date_time_test();	
+	return 0;
 }
-
-//void check_overflow( count ) const
-//{
-//    if( std::numeric_limits< time_type >::max() / count < m_msec_ratio )
-//    {
-//        throw std::invalid_argument{ "Overflow error" };
-//    }
-//}
