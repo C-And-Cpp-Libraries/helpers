10ec50b4ac2621cdc30d864e695987161740ee0f
Sergey Koparov <skoparov@kraftway.ru>
2017-05-04 19:19:21 +0300

    Remove overlooked typedefs

--- experimental/impl/time_ratio.h
+++ experimental/impl/time_ratio.h
@@ -112,14 +112,6 @@ operator-( const time_ratio< ltk, lp >& l, const time_ratio< rtk, rp >& r ) noex
 
 }// details
 
-using nanosec_t  = details::time_ratio< ratio::ns_ratio_sec,    1 >;
-using microsec_t = details::time_ratio< ratio::micro_ratio_sec, 1 >;
-using millisec_t = details::time_ratio< ratio::milli_ratio_sec, 1 >;
-using seconds_t  = details::time_ratio< ratio::sec_ratio_sec,   1 >;
-using minutes_t  = details::time_ratio< 1, ratio::min_ratio_sec >;
-using hours_t    = details::time_ratio< 1, ratio::hour_ratio_sec >;
-using days_t     = details::time_ratio< 1, ratio::day_ratio_sec >;
-
 
 }// temporal
 
