12c92bee6100facbb6e5fdc7a4bb0fd45c2d4272
Sergey Koparov <skoparov@kraftway.ru>
2017-05-22 16:18:55 +0300

    Add tests for test module

--- helper_tests/main.cpp
+++ helper_tests/main.cpp
@@ -1,5 +1,12 @@
-#define HELPERS_MAIN
-#include "temporal_tests.h"
-#include "thread_pool_tests.h"
-#include "polymorph_tests.h"
-#include "benchmarking_tests.h"
+#define TEST_TESTS 0
+
+#if !TEST_TESTS
+    #define HELPERS_MAIN
+    #include "temporal_tests.h"
+    #include "thread_pool_tests.h"
+    #include "polymorph_tests.h"
+    #include "benchmarking_tests.h"
+#else
+    #include "test_module_tests.h"
+    int main(){ tests_test(); }
+#endif
